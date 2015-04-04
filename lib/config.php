eba82a368c5d54ad7aa6f1c389b143176ee9c086
Edward Kim <onward.edkim@gmail.com>
2015-04-04 23:10:47 +1100

    remove sidebar form single page

--- lib/config.php
+++ lib/config.php
@@ -52,6 +52,8 @@ function display_sidebar() {
        *
        */
       [
+        'is_single',
+        'is_archive',
         'is_404',
         'is_front_page',
         ['is_page_template', 'template-custom.php']
